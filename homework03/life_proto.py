63646e9bba86142c94679bc21281796a5a97b6f9
ARTEMIDA7736 <112976222+ARTEMIDA7736@users.noreply.github.com>
2022-11-01 20:17:53 +0300

    Update life_proto.py

--- homework03/life_proto.py
+++ homework03/life_proto.py
@@ -30,14 +30,14 @@ class GameOfLife:
         self.speed = speed
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
+        """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def run(self) -> None:
-        """ Запустить игру """
+        """Запустить игру"""
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
@@ -49,7 +49,7 @@ class GameOfLife:
         running = True
         while running:
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == pygame.QUIT:
                     running = False
             self.draw_lines()
 
@@ -62,24 +62,21 @@ class GameOfLife:
         pygame.quit()
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        """
-        Создание списка клеток.
+        answer = []
+        if randomize == True:
+            for i in range(self.cell_height):
+                b = []
+                for j in range(self.cell_width):
+                    b.append(random.randint(0, 1))
+                answer.append(b)
+        else:
+            for i in range(self.cell_height):
+                b = []
+                for j in range(self.cell_width):
+                    b.append(0)
+                answer.append(b)
 
-        Клетка считается живой, если ее значение равно 1, в противном случае клетка
-        считается мертвой, то есть, ее значение равно 0.
-
-        Parameters
-        ----------
-        randomize : bool
-            Если значение истина, то создается матрица, где каждая клетка может
-            быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
-
-        Returns
-        ----------
-        out : Grid
-            Матрица клеток размером `cell_height` х `cell_width`.
-        """
-        pass
+        return answer
 
     def draw_grid(self) -> None:
         """
@@ -88,32 +85,79 @@ class GameOfLife:
         pass
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        """
-        Вернуть список соседних клеток для клетки `cell`.
-
-        Соседними считаются клетки по горизонтали, вертикали и диагоналям,
-        то есть, во всех направлениях.
-
-        Parameters
-        ----------
-        cell : Cell
-            Клетка, для которой необходимо получить список соседей. Клетка
-            представлена кортежем, содержащим ее координаты на игровом поле.
-
-        Returns
-        ----------
-        out : Cells
-            Список соседних клеток.
-        """
-        pass
+        if cell[0] == 0:
+            if cell[1] == 0:
+                return [self.grid[0][1], self.grid[1][0], self.grid[1][1]]
+            elif cell[1] == len(self.grid[0]) - 1:
+                return [self.grid[0][cell[1] - 1], self.grid[1][cell[1] - 1], self.grid[1][cell[1]]]
+            else:
+                return [
+                    self.grid[0][cell[1] - 1],
+                    self.grid[1][cell[1] - 1],
+                    self.grid[1][cell[1]],
+                    self.grid[1][cell[1] + 1],
+                    self.grid[0][cell[1] + 1],
+                ]
+        elif cell[0] == len(self.grid) - 1:
+            if cell[1] == 0:
+                return [self.grid[cell[0] - 1][0], self.grid[cell[0] - 1][1], self.grid[cell[0]][1]]
+            elif cell[1] == len(self.grid[0]) - 1:
+                return [
+                    self.grid[cell[0] - 1][cell[1] - 1],
+                    self.grid[cell[0]][cell[1] - 1],
+                    self.grid[cell[0] - 1][cell[1]],
+                ]
+            else:
+                return [
+                    self.grid[cell[0]][cell[1] - 1],
+                    self.grid[cell[0] - 1][cell[1] - 1],
+                    self.grid[cell[0] - 1][cell[1]],
+                    self.grid[cell[0] - 1][cell[1] + 1],
+                    self.grid[cell[0]][cell[1] + 1],
+                ]
+        else:
+            if cell[1] == 0:
+                return [
+                    self.grid[cell[0] - 1][0],
+                    self.grid[cell[0] - 1][1],
+                    self.grid[cell[0]][1],
+                    self.grid[cell[0] + 1][1],
+                    self.grid[cell[0] + 1][0],
+                ]
+            elif cell[1] == len(self.grid[0]) - 1:
+                return [
+                    self.grid[cell[0] - 1][cell[1]],
+                    self.grid[cell[0] - 1][cell[1] - 1],
+                    self.grid[cell[0]][cell[1] - 1],
+                    self.grid[cell[0] + 1][cell[1] - 1],
+                    self.grid[cell[0] + 1][cell[1]],
+                ]
+            else:
+                return [
+                    self.grid[cell[0] - 1][cell[1] - 1],
+                    self.grid[cell[0] - 1][cell[1]],
+                    self.grid[cell[0] - 1][cell[1] + 1],
+                    self.grid[cell[0]][cell[1] + 1],
+                    self.grid[cell[0] + 1][cell[1] + 1],
+                    self.grid[cell[0] + 1][cell[1]],
+                    self.grid[cell[0] + 1][cell[1] - 1],
+                    self.grid[cell[0]][cell[1] - 1],
+                ]
 
     def get_next_generation(self) -> Grid:
-        """
-        Получить следующее поколение клеток.
+        otvet = []
+        for i in range(self.cell_height):
+            row = []
+            for j in range(self.cell_width):
+                row.append(self.grid[i][j])
+            otvet.append(row)
+        for i in range(self.cell_height):
+            for j in range(self.cell_width):
+                if (
+                    sum(self.get_neighbours((i, j))) != 2 and sum(self.get_neighbours((i, j))) != 3
+                ) and self.grid[i][j] == 1:
+                    otvet[i][j] = 0  # RIP life dot((
+                elif sum(self.get_neighbours((i, j))) == 3 and self.grid[i][j] == 0:
+                    otvet[i][j] = 1  # newbie is here
 
-        Returns
-        ----------
-        out : Grid
-            Новое поколение клеток.
-        """
-        pass
+        return otvet
